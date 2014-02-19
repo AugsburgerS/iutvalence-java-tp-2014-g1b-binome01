f0c36921d7be173c4299ae16b7c46ccff420fcf9
augsburs <augsburs@PC-B101-05.iut-valence.fr>
2014-02-19 16:59:29 +0100

    Création de la classe suspect

---
+++ src/fr/iutvalence/java/tp/cluedo/Suspect.java
@@ -0,0 +1,17 @@
+package fr.iutvalence.java.tp.cluedo;
+
+/**
+ * Définition d'un suspect
+ * @author augsburs
+ */
+public class Suspect
+{
+	/**
+	 * Initialisation d'un suspect
+	 * @param nom nom du suspect
+	 */
+	public Suspect(String nom)
+	{
+		
+	}
+}
