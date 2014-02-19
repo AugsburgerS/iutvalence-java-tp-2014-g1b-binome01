2977a6ec6a937098d69c168e4368d6348ed92610
augsburs <augsburs@PC-B101-05.iut-valence.fr>
2014-02-19 16:46:35 +0100

    Création de la classe pièce

---
+++ src/fr/iutvalence/java/tp/cluedo/Piece.java
@@ -0,0 +1,17 @@
+package fr.iutvalence.java.tp.cluedo;
+
+/**
+ * Définition d'une pièce du manoir
+ * @author augsburs
+ */
+public class Piece
+{
+	/**
+	 * Création d'une pièce
+	 * @param nom
+	 */
+	public Piece(String nom)
+	{
+		
+	}
+}
