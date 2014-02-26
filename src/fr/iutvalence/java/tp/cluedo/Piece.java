e6217ed4c140a2164b88f0fa86c7fe8de817130f
augsburs <augsburs@PC-B101-05.iut-valence.fr>
2014-02-26 17:01:52 +0100

    Création des pièces

--- src/fr/iutvalence/java/tp/cluedo/Piece.java
+++ src/fr/iutvalence/java/tp/cluedo/Piece.java
@@ -6,5 +6,68 @@ package fr.iutvalence.java.tp.cluedo;
  */
 public class Piece
 {
-	//TODO Initialiser les différents types de pièces
+	/**
+	 * La pièce cuisine
+	 */
+	public static final int CUISINE = 0;
+
+	/**
+	 * La pièce chambre
+	 */
+	public static final int CHAMBRE = 1;
+
+	/**
+	 * La pièce salle de bains
+	 */
+	public static final int SALLE_DE_BAINS = 2;
+
+	/**
+	 * La pièce salle à manger
+	 */
+	public static final int SALLE_A_MANGER = 3;
+
+	/**
+	 * La pièce salle de jeux
+	 */
+	public static final int SALLE_DE_JEUX = 4;
+
+	/**
+	 * La pièce bibliothèque
+	 */
+	public static final int BIBLIOTHEQUE = 5;
+	
+	/**
+	 * La pièce bureau
+	 */
+	public static final int BUREAU = 6;
+
+	/**
+	 * La pièce hall
+	 */
+	public static final int HALL = 7;
+
+	/**
+	 * La pièce salon
+	 */
+	public static final int SALON = 8;
+
+	/**
+	 * La pièce cluedo
+	 */
+	public static final int CLUEDO = 9;
+
+	/**
+	 * Le type porte
+	 */
+	public static final int PORTE = 10;
+
+	/**
+	 * La case couloir
+	 */
+	public static final int COULOIR = 11;
+
+	/**
+	 * La case vide
+	 */
+	public static final int VIDE = 12;
 }
