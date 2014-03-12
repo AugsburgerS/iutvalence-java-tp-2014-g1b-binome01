f42bae33ff7cf6783ea68446aea86318cd4e4a5c
augsburs <augsburs@PC-B101-05.iut-valence.fr>
2014-03-12 17:02:17 +0100

    Remplissage du plateau en suivant le nouveau constructeur

--- src/fr/iutvalence/java/tp/cluedo/Case.java
+++ src/fr/iutvalence/java/tp/cluedo/Case.java
@@ -10,7 +10,7 @@ public class Case
 	 * Le type de la case
 	 */
 	private static Piece TYPE_CASE;
-	
+
 	/**
 	 * Type de la cloison au Nord de la case
 	 */
@@ -39,12 +39,37 @@ public class Case
 	 * @param est Type, de cloison au est de la case
 	 * @param ouest Type, de cloison au ouest de la case
 	 */
-	public Case(Piece typeCase, Cloison nord, Cloison sud, Cloison est, Cloison ouest)
+	public Case(Piece typeCase, Cloison nord, Cloison est, Cloison sud, Cloison ouest)
 	{
 		this.TYPE_CASE = typeCase;
 		this.NORD = nord;
 		this.SUD = sud;
 		this.EST = est;
 		this.OUEST = ouest;
+	}
+	
+	public static Piece obtenirTypeCase()
+	{
+		return TYPE_CASE;
+	}
+
+	public static Cloison obtenirNORD()
+	{
+		return NORD;
+	}
+
+	public static Cloison obtenirSUD()
+	{
+		return SUD;
+	}
+
+	public static Cloison obtenirEST()
+	{
+		return EST;
+	}
+
+	public static Cloison obtenirOUEST()
+	{
+		return OUEST;
 	}
 }
